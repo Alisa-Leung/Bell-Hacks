78ebe41473e13b2f84f320e88a54c7bfca3ecb4c
Alisa Leung <alisal.singyee@gmail.com>
2025-12-14 13:18:20 -0800

    cool particles

--- particles.js
+++ particles.js
@@ -13,9 +13,61 @@ const connectionDistance = 150;
 const particleSpeed = 0.5;
 class Particle {
     constructor(){
-        this.y = Math.random() * canvas.width;
+        this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
         this.vx = (Math.random() - 0.5) * particleSpeed;
-        this.vy = (Math.random())
+        this.vy = (Math.random() - 0.5) * particleSpeed;
+        this.radius = 2;
+    }
+    update(){
+        this.x += this.vx;
+        this.y += this.vy;
+        if (this.x < 0 || this.x > canvas.width) this.vx *= -1;
+        if (this.y < 0 || this.y > canvas.height) this.vy *= -1;
+    }
+    draw(){
+        ctx.fillStyle = "#EFE4D2";
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0,Math.PI*2);
+        ctx.fill();
     }
 }
+
+for (let i = 0; i < particleCount; i++) {
+    particles.push(new Particle());
+}
+
+function connectParticles() {
+    for (let i = 0; i < particles.length; i++) {
+        for (let j = i + 1; j < particles.length; j++) {
+            const dx = particles[i].x - particles[j].x;
+            const dy = particles[i].y - particles[j].y;
+            const dist = Math.sqrt(dx * dx + dy * dy);
+
+            if (dist < connectionDistance) {
+                const opacity = 1 - (dist / connectionDistance);
+                ctx.strokeStyle = `rgba(239, 228, 210, ${opacity * 0.3})`;
+                ctx.lineWidth = 1;
+                ctx.beginPath();
+                ctx.moveTo(particles[i].x, particles[i].y);
+                ctx.lineTo(particles[j].x, particles[j].y);
+                ctx.stroke();
+            }
+        }
+    }
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    particles.forEach(p => {
+        p.update();
+        p.draw();
+    });
+    
+    connectParticles();
+    
+    requestAnimationFrame(animate);
+}
+
+animate();
